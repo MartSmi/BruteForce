af63120f562dac428e45dcddebb7afdb13a965f6
Mike <mikas.pup@gmail.com>
2019-05-10 14:13:37 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let stepsTotal = 4;
-let currentStep = 0;
+let currentStep = 1;
 
 let prog = document.getElementById('progress');
 
@@ -10,11 +10,30 @@ let btnNext = document.getElementById('next');
 let btnBack = document.getElementById('back');
 
 $(btnBack).hide();
+$("#step2").hide();
+$("#step3").hide();
+$("#step4").hide();
+steps.innerHTML = "Step: " + currentStep;
 
+btnNext.onclick = function () {
 
-btn.onclick = function () {
   if (currentStep < 4) {
     currentStep++;
     steps.innerHTML = "Step: " + currentStep;
+  }
+
+  $(".steps").hide();
+  switch (currentStep) {
+    case 2:
+      $("#step2").show();
+    break;
+
+    case 3:
+      $("#step3").show();
+    break;
+
+    case 4:
+      $("#step4").show();
+    break;
   }
 }
